3b6021631735fc1212b68d0dc52f96a091baf26b
Lt_Lefty15 <lschaut@my.heartland.edu>
2020-01-19 16:34:30 -0600

    Merge

--- Assets/Script1/Rift_Animate.cs
+++ Assets/Script1/Rift_Animate.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Rift_Animate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+	public Sprite frame1, frame2, frame3, frame4
+	public double framerate
+	
+	// Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent(SpriteRenderer).sprite = newSprite;
     }
 
     // Update is called once per frame
@@ -15,4 +19,5 @@ public class Rift_Animate : MonoBehaviour
     {
         
     }
+	
 }
