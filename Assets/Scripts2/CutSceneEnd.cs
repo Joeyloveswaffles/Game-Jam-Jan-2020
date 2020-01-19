62af827e21ad79dac5eba60675cd231badbec7ee
Niko R <nikorizo14@gmail.com>
2020-01-19 15:10:23 -0600

    added things

---
+++ Assets/Scripts2/CutSceneEnd.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutSceneEnd : MonoBehaviour
+{
+    public int firstLevelBuildIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(jumpToScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator jumpToScene()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(firstLevelBuildIndex);
+    }
+}
