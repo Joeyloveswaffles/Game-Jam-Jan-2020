3b147386a04b6155d9a94e88b3fbde840daae527
swagking12343 charles <jake.m.botka@gmail.com>
2020-01-19 14:29:16 -0600

    new

---
+++ Assets/Gun_Animation_Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun_Animation_Manager : MonoBehaviour
+{
+    public Gun gun;
+    public Sprite muzzleFlash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    public IEnumerator shootFlash()
+    {
+        yield return new WaitForSeconds(1f);
+
+    }
+}
